e8c8d0e649ab0b2798ff6090664347371dd4ec2b
KeerthiR02 <158005399+KeerthiR02@users.noreply.github.com>
2024-03-17 23:14:10 -0400

    Update countusingpython.py

--- countusingpython.py
+++ countusingpython.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import datetime
 
-uri = "mongodb+srv://keerthi2556:Reddy2002@cluster0.3wm2fac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = mongodb+srv://keerthi2556:Reddy2002@cluster0.eaiaoy5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
