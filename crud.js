c2f01e3a1f4852a36dcea5f08bd541945736f840
KeerthiR02 <158005399+KeerthiR02@users.noreply.github.com>
2024-03-25 12:52:26 -0400

    crud.js

--- crud.js
+++ crud.js
@@ -1,10 +1,10 @@
-#to create a new collection
+
 db.createCollection("users")
 
-#create_operation to insert a single document into a collection
+#to insert a single document into a collection
 db.users.insertOne({name: "Rupa", age: 25,});
 
-#create_operation to insert multiple documents into a collection
+ #to insert multiple documents into a collection
 db.users.insertMany([
     {
         name: "Rupa",
@@ -44,13 +44,51 @@ db.users.deleteMany({ age: { $lt: 30 } })
 
 #updateOne
 db.podcasts.updateOne(
-  {title: "The Developer Hub"}, // Specify the query to find the document by its title
+  {title: "The Developer Hub"}, 
   {$set: {topics: ["databases", "MongoDB", "MongoDB Compass"]}}, 
   {upsert: true} 
 );
 
-#count.js for counting trip duration
+#count
 db.trips.countDocuments();
 db.trips.countDocuments({tripduration: {$gt: 200}, usertype: "Subscriber"});
+
+#findandModify
+db.podcasts.findAndModify({
+  query: {_id: ObjectId("65c45e54c6d23febaede0df0")},
+  update: {$inc: {downloads: 1}},
+  new: true,
+});
+
+#push.js
+db.podcasts.updateOne(
+  {_id: ObjectId("65c3b31c5f2b7a48feaa52e0")},
+  {$push: {platforms: "Apple Podcast"}}
+);
+
+db.podcasts.findOne({title: "The Developer Hub"});
+
+
+#replaceOne
+db.books.replaceOne(
+  {_id: ObjectId("65c3f41508ac0649ab5ae43b")}, 
+  {
+    _id: ObjectId("65c3f41508ac0649ab5ae43b"),
+    title: "The Art of War",
+    ISBN: "978-1590302255",
+    thumbnailUrl: "https://example.com/art-of-war-thumbnail.jpg",
+    publicationDate: ISODate("2020-01-01T00:00:00.000Z"),
+    authors: ["Sun Tzu"],
+  }
+);
+
+db.books.findOne({_id: ObjectId("65c3f41508ac0649ab5ae43b")});
+
+
+
+
+
+
+
 
 
